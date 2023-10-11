aef86ff296e418de069f6c395c9f6f7aa2cc1f72
Sid1772 <sidbhatia60@gmail.com>
2023-10-11 12:10:44 +0530

    Added Dropdwon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
+    WeatherComponent,
     AppComponent,
-    WeatherComponent
+    
   ],
   imports: [
+    NgSelectModule,
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,80 +1,107 @@
 import { Component, OnInit } from '@angular/core';
 import { Cities } from '../cities';
 import { WeatherService } from './weather.service';
-import {take} from 'rxjs/operators'
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css','./weathericons.css']
+  styleUrls: ['./weather.component.css', './weathericons.css'],
 })
 export class WeatherComponent implements OnInit {
-selectedCity:string=""
-currentWeather:any={}
-fetchingData=true;
-forecastWeather:Array<{icon:string,id:string,temp:string,max:string,min:string,date:Date}>=[]
-citiesData:Array<Cities>=[]
-  constructor(private service:WeatherService) { }
-
+  selectedCity: string = '';
+  selectedCityId: number = 0;
+  currentWeather: any = {};
+  fetchingData = true;
+  dropdownProperties: any = {
+    virtualScroll: true,
+    typeahead: true,
+    searchable: true,
+    clearable: false,
+  };
+  forecastWeather: Array<{
+    icon: string;
+    id: string;
+    temp: string;
+    max: string;
+    min: string;
+    date: Date;
+  }> = [];
+  citiesData: Array<Cities> = [];
+  constructor(private service: WeatherService) {}
   ngOnInit(): void {
-    this.getCitiesData() 
+    this.getCitiesData();
   }
-  getCitiesData(){
-    this.service.getCitiesData().subscribe(
-      (city:any)=>{
-        this.citiesData=city
-        this.getWeatherData(this.citiesData[0])
-      }
-    )
+  changeCity() {
+    let city = this.citiesData.filter((c) => c.id == this.selectedCityId)[0];
+    this.getWeatherData(city);
   }
-  getWeatherData(city:Cities){
-    this.fetchingData=true
-    this.selectedCity=city.nm
+  getCitiesData() {
+    this.service.getCitiesData().subscribe((city: any) => {
+      this.citiesData = city;
+      this.getWeatherData(this.citiesData[0]);
+    });
+  }
+  getWeatherData(city: Cities) {
+    this.fetchingData = true;
+    //Assigning City
+    this.selectedCity = city.nm;
+    this.selectedCityId = city.id;
     this.getCurrentWeather(city);
-    this.getForecast(city)
-   
-    
+    this.getForecast(city);
   }
-  getCurrentWeather({lat,lon}:{lat:number,lon:number}){
-    this.service.getCurrentWeather(lat,lon).subscribe((cw:any)=>{
-      console.log(cw)
-      this.currentWeather={icon:cw.weather[0].icon,id:`wi-icon-`+cw.weather[0].id,temp:cw.main.temp}
-    })
+  getCurrentWeather({ lat, lon }: { lat: number; lon: number }) {
+    this.service.getCurrentWeather(lat, lon).subscribe((cw: any) => {
+      this.currentWeather = {
+        icon: cw.weather[0].icon,
+        id: `wi-icon-` + cw.weather[0].id,
+        temp: cw.main.temp,
+      };
+    });
   }
-  getForecast({lat,lon}:{lat:number,lon:number}){
-    this.service.getWeatherForecast(lat,lon).subscribe((fw:any)=>{
-      console.log(fw)
-      this.setForecastWeather(fw.list)
-      this.fetchingData=false
-    })
+  getForecast({ lat, lon }: { lat: number; lon: number }) {
+    this.service.getWeatherForecast(lat, lon).subscribe((fw: any) => {
+      console.log(fw);
+      this.setForecastWeather(fw.list);
+      this.fetchingData = false;
+    });
   }
-  setForecastWeather(list:Array<any>){
-    let dateTimeData:any={}
-    list.forEach((data:any)=>{
-      let date=data.dt_txt.split(" ")[0]
-      let dataTobePushed={date:data.dt,min:data.main.temp_min,max:data.main.temp_max,icon:data.weather[0].icon,id:`wi-icon-`+data.weather[0].id}
-      dateTimeData[date]?dateTimeData[date].push(dataTobePushed):dateTimeData[date]=[dataTobePushed]
-    })
-    console.log(dateTimeData)
-    let maxDataTimeData:any=[]
-    for(let date of Object.keys(dateTimeData)){
-      let max=this.getMax(dateTimeData[date], 'max');
-      let min= this.getMin(dateTimeData[date], 'min');
-      maxDataTimeData.push({max,min,date,icon:dateTimeData[date][0].icon,id:dateTimeData[date][0].id})
+  setForecastWeather(list: Array<any>) {
+    let dateTimeData: any = {};
+    list.forEach((data: any) => {
+      let date = data.dt_txt.split(' ')[0];
+      let dataTobePushed = {
+        date: data.dt,
+        min: data.main.temp_min,
+        max: data.main.temp_max,
+        icon: data.weather[0].icon,
+        id: `wi-icon-` + data.weather[0].id,
+      };
+      dateTimeData[date] = dateTimeData[date]
+        ? [...dateTimeData[date], dataTobePushed]
+        : [dataTobePushed];
+    });
+    console.log(dateTimeData);
+    let maxDataTimeData: any = [];
+    for (let date of Object.keys(dateTimeData)) {
+      let max = this.getMax(dateTimeData[date], 'max');
+      let min = this.getMin(dateTimeData[date], 'min');
+      maxDataTimeData.push({
+        max,
+        min,
+        date,
+        icon: dateTimeData[date][0].icon,
+        id: dateTimeData[date][0].id,
+      });
     }
-    console.log(maxDataTimeData)
-   this.forecastWeather=maxDataTimeData.slice(1,4)
+    console.log(maxDataTimeData);
+    //For Getting next 3 days Data only
+    this.forecastWeather = maxDataTimeData.slice(1, 4);
   }
-   getMax(arr:any, attr:any){
-    return Math.max(...arr.map((item:any) => item[attr]));
+  getMax(arr: any, attr: any) {
+    return Math.max(...arr.map((item: any) => item[attr]));
   }
-  
-   getMin(arr:any, attr:any){
-    return Math.min(...arr.map((item:any) => item[attr]));
+  getMin(arr: any, attr: any) {
+    return Math.min(...arr.map((item: any) => item[attr]));
   }
-  changeCity(cityId:number){
-    let city=this.citiesData.filter(c=>c.id==cityId)[0]
-    this.getWeatherData(city)
-  }
-
 }
